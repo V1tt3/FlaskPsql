47e5802764ae7470fe85259e316081b843050b8a
V1tt3 <romario2000@bk.ru>
2021-03-26 18:25:49 +0300

    gg

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from config import Configurations 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config.from_object(Configurations)
 
-
+db = SQLAlchemy()
 
 
 
