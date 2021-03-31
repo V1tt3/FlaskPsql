dbedb43b103cc6d46e8affec5308e58080cb243e
V1tt3 <romario2000@bk.ru>
2021-03-31 21:24:59 +0300

    Add registration

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import db, manager
+from app import db
 from flask_login import UserMixin
 
 class Users(db.Model, UserMixin):
--- view.py
+++ view.py
@@ -2,7 +2,7 @@ from app import app
 
 
 from flask import render_template, request, redirect, url_for, flash, session
-from models import Users
+from models import *
 from forms import *
 
 
@@ -12,11 +12,24 @@ from forms import *
 def index():
     return render_template("index.html")
 
+#REGISTRATION
 @app.route("/registration", methods=['GET', 'POST'])
 def registration():
     reg_form = RegForm()
     if reg_form.validate_on_submit():
-        return "great"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        #username exist???
+        user_object = Users.query.filter_by(username = username).first()
+        if user_object:
+            return "Someone else has that username!"
+
+        # add user in db
+        user = Users(username = username, password = password)
+        db.session.add(user)
+        db.session.commit()
+        return 'Now you are in db'
 
     return render_template("registration.html", form = reg_form)
 
