62730953437f230647805e0827ad2af4acceb2f7
V1tt3 <romario2000@bk.ru>
2021-04-06 15:53:39 +0300

    flask-login

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from config import Configurations 
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_manager
+from flask_login import LoginManager
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
@@ -14,8 +14,8 @@ app.config.from_object(Configurations)
 
 db = SQLAlchemy(app)
 
-#log_manager = login_manager(app)
-
+login_manager = LoginManager(app)
+login_manager.init_app(app)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
--- view.py
+++ view.py
@@ -1,12 +1,15 @@
-from app import app
+from app import app, login_manager
 
 
 from flask import render_template, request, redirect, url_for, flash, session
+from flask_login import login_user, logout_user, current_user, login_required
 from models import *
 from forms import *
 
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
 
 @app.route("/")
 def index():
@@ -36,11 +39,20 @@ def login():
     log_form = LogForm()
     
     if log_form.validate_on_submit():
-        return "Logged in!"
+
+        username = log_form.username.data
+        user = Users.query.filter_by(username = username).first()
+        login_user(user)
+        if current_user.is_authenticated:
+            return ('Success!!, now you are logged in')
+        else:
+            return ('Not logged in')
 
     return render_template("login.html", form = log_form)
     
 
-@app.route("/logout", methods = ['GET, POST'])
+@app.route("/logout", methods = ['GET'])
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('login'))
